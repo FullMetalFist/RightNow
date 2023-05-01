798fdea508d788e72b28c4ebab94b29477b99030
FullMetalFist <m.vilabrera@gmail.com>
2023-05-01 11:02:34 -0400

    Displaying cell info

---
+++ RightNow/Models/Date+Today.swift
@@ -0,0 +1,30 @@
+//
+//  Date+Today.swift
+//  RightNow
+//
+//  Created by Michael Vilabrera on 5/1/23.
+//
+
+import Foundation
+
+extension Date {
+    var dayAndTimeText: String {
+        let timeText = formatted(date: .omitted, time: .shortened)
+        if Locale.current.calendar.isDateInToday(self) {
+            let timeFormat = NSLocalizedString("Today at %@", comment: "Today at time format string")
+            return String(format: timeFormat, timeText)
+        } else {
+            let dateText = formatted(.dateTime.month(.abbreviated).day())
+            let dateAndTimeFormat = NSLocalizedString("%@ at %@", comment: "Date and time time format string")
+            return String(format: dateAndTimeFormat, dateText, timeText)
+        }
+    }
+    
+    var dayText: String {
+        if Locale.current.calendar.isDateInToday(self) {
+            return NSLocalizedString("Today", comment: "Today due date description")
+        } else {
+            return formatted(.dateTime.month().day().weekday(.wide))
+        }
+    }
+}
