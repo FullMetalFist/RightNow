9cc78f13abc08c1ceb7d144bea7e02d557ff86d8
FullMetalFist <m.vilabrera@gmail.com>
2023-05-11 21:42:58 -0400

    Using content views

---
+++ RightNow/ContentViews/UIContentConfiguration+Stateless.swift
@@ -0,0 +1,15 @@
+//
+//  UIContentConfiguration+Stateless.swift
+//  RightNow
+//
+//  Created by Michael Vilabrera on 5/11/23.
+//
+
+import UIKit
+
+extension UIContentConfiguration {
+    
+    func updated(for state: UIConfigurationState) -> Self {
+        return self
+    }
+}
